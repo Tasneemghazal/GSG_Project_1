7a0f52e4a26c6fd1e8c46ca503fefa7f2f68a9af
normaIyad <normamohsan39@gmail.com>
2025-02-04 18:58:26 +0200

    ONLINE APPOINMENT section

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -2,6 +2,7 @@ import Navbar from "../Components/HomeComponents/Nav/Navbar";
 import Slider from "../Components/HomeComponents/Slider/Slider";
 import About from "../Components/HomeComponents/About/About";
  import BeastServices from "../Components/HomeComponents/beast_service/BeastServices";
+import Make_apponment from "../Components/HomeComponents/make_apponment/Make_apponment";
  const Home = () => {
   return (
     <>
@@ -9,6 +10,7 @@ import About from "../Components/HomeComponents/About/About";
     <Slider/>
     <About/>
     <BeastServices/>
+    <Make_apponment/>
     </>
   )
 }
