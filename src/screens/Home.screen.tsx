9a524c770ede2572b96a04f391815493d4f177d8
normaIyad <normamohsan39@gmail.com>
2025-02-05 20:40:31 +0200

    footer part

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,16 +1,21 @@
 import Navbar from "../Components/HomeComponents/Nav/Navbar";
 import Slider from "../Components/HomeComponents/Slider/Slider";
 import About from "../Components/HomeComponents/About/About";
- import BeastServices from "../Components/HomeComponents/beast_service/BeastServices";
+import BeastServices from "../Components/HomeComponents/beast_service/BeastServices";
 import Make_apponment from "../Components/HomeComponents/make_apponment/Make_apponment";
- const Home = () => {
+import Footer from "../Components/HomeComponents/Footer/Footer";
+import Testmonial from "../Components/HomeComponents/Testmanial/Testmonial";
+
+const Home = () => {
   return (
     <>
-    <Navbar/>
-    <Slider/>
-    <About/>
-    <BeastServices/>
-    <Make_apponment/>
+      <Navbar />
+      <Slider />
+      <About />
+      <BeastServices />
+      <Make_apponment />
+      <Testmonial/>
+      <Footer />
     </>
   )
 }
