9de87ab9edc59c3e37f424fccf3da2b93ac8e2fd
Tasneem <tasneemgazal5@gmail.com>
2025-02-16 13:31:53 +0200

    fix naming issue

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -1,9 +1,9 @@
-import Navbar from "../../Components/HomeComponents/Nav/Navbar";
-import Slider from "../../Components/HomeComponents/Slider/Slider";
-import About from "../../Components/HomeComponents/About/About";
-import BeastServices from "../../Components/HomeComponents/beast_service/BeastServices";
-import Make_appointment from "../../Components/HomeComponents/make_appointment/Make_Appointment";
-import Footer from "../../Components/HomeComponents/Footer/Footer";
+import Navbar from "../../components/homeComponents/Nav/Navbar";
+import Slider from "../../components/homeComponents/Slider/Slider";
+import About from "../../components/homeComponents/About/About";
+import BeastServices from "../../components/homeComponents/beast_service/BeastServices";
+import Make_appointment from "../../components/homeComponents/make_appointment/Make_Appointment";
+import Footer from "../../components/homeComponents/Footer/Footer";
 
 const Home = () => {
   return (
